fe81b9db25dbb99cacc5210896016f42df923d47
Xaoka <powellrowan@yahoo.co.uk>
2020-05-11 15:07:06 +0100

    Updates

---
+++ precache-manifest.ada4c0bd3ed81c3af930a29a1b031d46.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9729c7739c576870bc98b8f6be843c2e",
+    "url": "/yoello-react/index.html"
+  },
+  {
+    "revision": "d58ffb61698455a04b44",
+    "url": "/yoello-react/static/css/main.1609092c.chunk.css"
+  },
+  {
+    "revision": "c7f20e27c25d279bed7d",
+    "url": "/yoello-react/static/js/2.f82ed12c.chunk.js"
+  },
+  {
+    "revision": "801455085fc6f57b23eb009849543052",
+    "url": "/yoello-react/static/js/2.f82ed12c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d58ffb61698455a04b44",
+    "url": "/yoello-react/static/js/main.e5d6ca5a.chunk.js"
+  },
+  {
+    "revision": "e4664a4144e495166041",
+    "url": "/yoello-react/static/js/runtime-main.89556bfe.js"
+  },
+  {
+    "revision": "2c5e59aada106143d09417375f82fbfe",
+    "url": "/yoello-react/static/media/coffee.2c5e59aa.png"
+  },
+  {
+    "revision": "03ed126168c903e95ab55f01d9abd6b3",
+    "url": "/yoello-react/static/media/cutlery.03ed1261.png"
+  }
+]);
